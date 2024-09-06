7a05564410b88e9ff154a6c11e4b1926148a3b7f
Steven Rockarts <steven@airtrail.co>
2024-09-06 12:26:32 -0600

    Try calculating hash

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,12 @@
 import com.google.gson.Gson
-import java.math.BigInteger
 import com.dampcake.bencode.Bencode
 import com.dampcake.bencode.Type
 import java.io.File
-import java.io.FileInputStream
+import java.security.MessageDigest
+
 val gson = Gson()
 
 fun main(args: Array<String>) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    //println("Logs from your program will appear here!")
     val command = args[0]
     when (command) {
         "info" -> {
@@ -28,13 +26,28 @@ fun main(args: Array<String>) {
 fun decodeTorrentFile(torrentFile: String) {
     val fileBytes = File(torrentFile).readBytes()
     val bencode = Bencode()
-    val torrentData = bencode.decode(fileBytes, Type.DICTIONARY)
+    val torrentData = bencode.decode(fileBytes, Type.DICTIONARY) as Map<String, Any>
     val url = torrentData["announce"]
-    val info = torrentData["info"] as MutableMap<*, *>
+    val info = torrentData["info"] as Map<*, *>
+
+    val encoded:ByteArray = bencode.encode(info)
+    val infoData = bencode.decode(encoded, Type.DICTIONARY) as Map<String, Any>
+    println(gson.toJson(infoData))
+ val json = gson.toJson(infoData)
+    
+    val bytes = MessageDigest.getInstance("SHA-1").digest(json.toByteArray())
+    val result = StringBuilder(bytes.size * 2)
+    val HEX_CHARS = "0123456789abcdef"
+    bytes.forEach {
+        val i = it.toInt()
+        result.append(HEX_CHARS[i shr 4 and 0x0f])
+        result.append(HEX_CHARS[i and 0x0f])
+    }
     val length = info["length"]
 
     println("Tracker URL: $url")
     println("Length: $length")
+    println("Info Hash: $result")
 }
 fun decodeBencode(benCodedValue: String): Any {
     val bencode = Bencode()
@@ -44,7 +57,5 @@ fun decodeBencode(benCodedValue: String): Any {
         benCodedValue[0] == 'l' -> bencode.decode(benCodedValue.toByteArray(), Type.LIST)
         benCodedValue[0] == 'd' -> bencode.decode(benCodedValue.toByteArray(), Type.DICTIONARY)
         else -> return ""
-
     }
-
 }
